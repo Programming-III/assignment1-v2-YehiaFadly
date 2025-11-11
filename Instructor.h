99e723113a4b80d12bf6fe1ee006d4912ff6014a
YehiaFadly <y25798021@gmail.com>
2025-11-11 10:09:34 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -2,18 +2,19 @@
 #define INSTRUCTOR_H
 
 #include "Person.h"
+#include <string>
+#include <iostream>
 using namespace std;
 //#write Instructor class here
 
-
-
-
-
-
-
-
-
-
+class Instructor: public Person{
+    private:
+    string department;
+    int experienceYears;
+    public:
+    Instructor(string n, int d, string dept, int exp) : Person(n, d);
+    void display();
+};
 
 
 #endif
