fe0632c96edd482844b9d204f0efd654d99bd29b
YehiaFadly <y25798021@gmail.com>
2025-11-11 10:11:44 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -3,19 +3,26 @@
 
 #include "Person.h"
 #include <string>
-#using namespace std;
-//#write student class here
+#include <iostream>
 
+#endif
+// Student.h - declaration only. Implementations are in main.cpp per assignment.
 
+class Student : public Person {
+private:
+    int yearLevel;
+    std::string major;
+public:
+    Student();
+    Student(const std::string& name, int id, int yearLevel, const std::string& major);
+    ~Student();
 
+    void setYearLevel(int y);
+    int getYearLevel() const;
+    void setMajor(const std::string& m);
+    std::string getMajor() const;
 
-
-
-
-
-
-
-
-
+    void display() const override;
+};
 
 #endif
